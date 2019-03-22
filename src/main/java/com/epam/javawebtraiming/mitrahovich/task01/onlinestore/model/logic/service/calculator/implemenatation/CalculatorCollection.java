75b4add9b0bac4f8c5156de190f8d43f9fec3856
Mihail <bladesvet@gmail.com>
2019-03-22 18:11:58 +0300

    refarcting UML, change log properties file

--- src/main/java/com/epam/javawebtraiming/mitrahovich/task01/onlinestore/model/logic/service/calculator/implemenatation/CalculatorCollection.java
+++ src/main/java/com/epam/javawebtraiming/mitrahovich/task01/onlinestore/model/logic/service/calculator/implemenatation/CalculatorCollection.java
@@ -2,12 +2,15 @@ package com.epam.javawebtraiming.mitrahovich.task01.onlinestore.model.logic.serv
 
 import java.math.BigDecimal;
 
+import org.apache.log4j.Logger;
+
 import com.epam.javawebtraiming.mitrahovich.task01.onlinestore.dao.DaoBase;
 import com.epam.javawebtraiming.mitrahovich.task01.onlinestore.model.entity.base.Base;
 import com.epam.javawebtraiming.mitrahovich.task01.onlinestore.model.entity.device.abstractentity.Device;
 import com.epam.javawebtraiming.mitrahovich.task01.onlinestore.model.logic.service.calculator.Calculator;
 
 public class CalculatorCollection<T extends Device> implements Calculator<T> {
+	private static final Logger log = Logger.getRootLogger();
 
 	@Override
 	public BigDecimal sumPrice(DaoBase<T> base) {
@@ -16,9 +19,11 @@ public class CalculatorCollection<T extends Device> implements Calculator<T> {
 		for (T dev : tempBase.getBase()) {
 			if (dev != null) {
 				sum = sum.add(dev.getPrice());
+				log.trace("device-" + dev.getClass().getSimpleName() + " ,price: " + dev.getPrice());
 			}
 		}
-
+		log.trace("sum: " + sum);
 		return sum;
+
 	}
 }
